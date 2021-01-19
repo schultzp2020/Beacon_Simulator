2ea692600594d7e124abeafeeb83a720d644c78e
Paul Schultz <pschultz@menard-inc.com>
2021-01-19 15:17:37 -0600

    Initial Commit

--- beacon.py
+++ beacon.py
@@ -1,27 +1,33 @@
 import argparse
 import requests
 import time
+import numpy as np
 
-def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
-    percent = ("{0:." + str(decimals) + "f}").format(100 *
-                                                     (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 *(iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=print_end)
 
     if iteration == total:
         print()
 
 def check_status_code(r):
     if r.status_code == 200:
+        if args.random:
+            mean = args.interval
+            scale = mean * 0.16
+            interval = int(np.abs(round(np.random.normal(mean, scale), 0)))
+        else: 
+            interval = args.interval
         if args.verbose:
             print("Successfully sent the payload!\n")
-            print(f"Waiting for {args.interval} seconds...")
-            print_progress_bar(0, args.interval)
-        for i in range(args.interval):
+            print(f"Waiting for {interval} seconds...")
+            print_progress_bar(0, interval)
+        for i in range(interval):
             time.sleep(1)
             if args.verbose:
-                print_progress_bar(i+1, args.interval)
+                print_progress_bar(i+1, interval)
         if args.verbose:
             print(f"Done waiting!\n")
     else:
@@ -86,11 +92,17 @@ if __name__ == '__main__':
     parser.add_argument("domain", nargs='?', help="Enter a domain to act as a C2.", type=str)
     parser.add_argument("-i", "--interval", help="Enter a time interval in seconds.", type=int)
     parser.add_argument("-v", "--verbose", help="Verbose mode: displays when the next packet will be sent.", action="store_true", default=False)
-    parser.add_argument("-V", "--version", help="Print out version and exit.", action="store_true")
+    parser.add_argument("-V", "--version", help="Print out version and exit.", action="store_true", default=False)
+    parser.add_argument("-r", "--random", help="Randomizes the sleep interval for the beacon using normal distribution.", action="store_true", default=False)
     args = parser.parse_args()
     if args.version:
         print("Beacon Sumulator version 1.0")
         exit(0)
+    if not args.domain:
+        print("Please enter a domain to act as a C2.")
+        exit(1)
+    if args.interval < 1: 
+        print("Please enter a time interval greater than zero.")
     if not args.interval:
         args.interval = 60
 
